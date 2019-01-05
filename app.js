96262eb31e1f7e78fc73965129d010ae7db06799
MikeKowalsky <mkowalewski83@gmail.com>
2019-01-05 20:46:02 +0100

    added UI class with display and add books  methods

--- app.js
+++ app.js
@@ -8,6 +8,40 @@ class Book {
 }
 
 // UI Class: Handle UI Tasks
+class UI {
+  static displayBooks() {
+    const StoredBooks = [
+      {
+        title: "Book One",
+        author: "John Doe",
+        isbn: "34234111234234"
+      },
+      {
+        title: "Book Two",
+        author: "Jane Doe",
+        isbn: "342342123134234"
+      }
+    ];
+
+    const books = StoredBooks;
+
+    books.forEach(book => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector("#book-list");
+
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    `;
+
+    list.appendChild(row);
+  }
+}
 
 // Store Class: Handles Storage
 
