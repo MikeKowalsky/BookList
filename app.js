99539dd50e4520839b5e82e42e49aec5c25ef149
MikeKowalsky <mkowalewski83@gmail.com>
2019-01-06 12:47:51 +0100

    added simple form validation and UI alerts

--- app.js
+++ app.js
@@ -53,6 +53,18 @@ class UI {
     document.querySelector("#author").value = "";
     document.querySelector("#isbn").value = "";
   }
+
+  static showAlert(message, className) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const form = document.querySelector("#book-form");
+    container.insertBefore(div, form);
+
+    // Vanish in 3 seconds
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
+  }
 }
 
 // Store Class: Handles Storage
@@ -70,19 +82,30 @@ document.querySelector("#book-form").addEventListener("submit", e => {
   const author = document.querySelector("#author").value;
   const isbn = document.querySelector("#isbn").value;
 
-  // Instantiste book
-  const book = new Book(title, author, isbn);
+  // Validate
+  if (title === "" || author === "" || isbn === "") {
+    UI.showAlert("Please fill in all fields", "danger");
+  } else {
+    // Instantiste book
+    const book = new Book(title, author, isbn);
 
-  console.log(book);
+    console.log(book);
 
-  // Add Book to UI
-  UI.addBookToList(book);
+    // Add Book to UI
+    UI.addBookToList(book);
 
-  // Clear fields
-  UI.clearFields();
+    // Show success message
+    UI.showAlert("Book Added", "success");
+
+    // Clear fields
+    UI.clearFields();
+  }
 });
 
 // Event: Remove a Book
 document.querySelector("#book-list").addEventListener("click", e => {
   UI.deleteBook(e.target);
+
+  // Show success message
+  UI.showAlert("Book Removed", "success");
 });
